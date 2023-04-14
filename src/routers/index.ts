f64478d0804555764162b525a49885ca65865e95
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-04-14 20:05:41 -0300

    Create index.ts

---
+++ src/routers/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import userRouter from "./user.router";
+import postRouter from "./post.router";
+
+const router = Router();
+
+router.use("/users", userRouter);
+router.use("/post", postRouter);
