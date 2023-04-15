589e90f9a87544d375537ca74fdbe42b0c52583b
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-04-14 23:43:17 -0300

    Update user.router.ts

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { authorizationMiddleware } from "../middlewares/authorization.middleware";
 import UsersService from "../services/users.service";
 
 const router = Router();
@@ -12,33 +13,61 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-router.get("/list", async (req: Request, res: Response) => {
-  const users = await UsersService.getAll();
-  res.send(users);
-});
-
-router.get("/:username", async (req: Request, res: Response) => {
-  const user = await UsersService.getByUsername(req.params.username);
-  if (!user)
-    return res.status(400).send({ message: "Usuário Não Encontrado!" });
-  res.status(200).send(user);
-});
-
-router.delete("/remove/:username", async (req: Request, res: Response) => {
-  try {
-    await UsersService.remove(req.params.username);
-    res.status(200).send({ message: "Usuário removido com sucesso!" });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
+router.get(
+  "/list",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    const users = await UsersService.getAll();
+    res.send(users);
   }
-});
+);
 
-router.put("/:username", async (req: Request, res: Response) => {
+router.get(
+  "/:username",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    const user = await UsersService.getByUsername(req.params.username);
+    if (!user)
+      return res.status(400).send({ message: "Usuário Não Encontrado!" });
+    res.status(200).send(user);
+  }
+);
+
+router.delete(
+  "/remove/:username",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      await UsersService.remove(req.params.username);
+      res.status(200).send({ message: "Usuário removido com sucesso!" });
+    } catch (error: any) {
+      res.status(400).send({ message: error.message });
+    }
+  }
+);
+
+router.put(
+  "/:username",
+  authorizationMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      await UsersService.update(req.params.username, req.body);
+      res.status(200).send({ message: "Usuário Atualizado com Sucesso!" });
+    } catch (err: any) {
+      res.status(400).send({ message: err.message });
+    }
+  }
+);
+
+router.post("/auth/login", async (req: Request, res: Response) => {
   try {
-    await UsersService.update(req.params.username, req.body);
-    res.status(200).send({ message: "Usuário Atualizado com Sucesso!" });
+    const token = await UsersService.authorization(
+      req.body.username,
+      req.body.password
+    );
+    res.status(200).send({ token });
   } catch (err: any) {
-    res.status(400).send({ message: err.message });
+    res.status(401).send({ message: err.message });
   }
 });
 
