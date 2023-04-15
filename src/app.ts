575639d1fd2a7fd0a768cc9909f72c008caa309a
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-04-14 21:59:57 -0300

    Update app.ts

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routers";
+import connection from "./config/database";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const port = 3000;
+
+connection
+  .then(() => {
+    console.log("Banco de Dados Conectado com Sucesso!");
+    app.listen(port, () => {
+      console.log("Aplicação online na porta: ", port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
