48a2295591304356fba563ecea821a496e140108
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-04-14 22:00:20 -0300

    Update user.router.ts

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,45 @@
 import { Request, Response, Router } from "express";
+import UsersService from "../services/users.service";
 
 const router = Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await UsersService.create(req.body);
+    res.send({ message: "Usuário Criado com Sucesso!" });
+  } catch (err: any) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+router.get("/list", async (req: Request, res: Response) => {
+  const users = await UsersService.getAll();
+  res.send(users);
+});
+
+router.get("/:username", async (req: Request, res: Response) => {
+  const user = await UsersService.getByUsername(req.params.username);
+  if (!user)
+    return res.status(400).send({ message: "Usuário Não Encontrado!" });
+  res.status(200).send(user);
+});
+
+router.delete("/remove/:username", async (req: Request, res: Response) => {
+  try {
+    await UsersService.remove(req.params.username);
+    res.status(200).send({ message: "Usuário removido com sucesso!" });
+  } catch (error: any) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+router.put("/:username", async (req: Request, res: Response) => {
+  try {
+    await UsersService.update(req.params.username, req.body);
+    res.status(200).send({ message: "Usuário Atualizado com Sucesso!" });
+  } catch (err: any) {
+    res.status(400).send({ message: err.message });
+  }
+});
 
 export default router;
