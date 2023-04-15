73d3b5b5fef2c193b1d3141ec21bc040f556be7b
Emerson Carvalho <emersoncarvalhodev@gmail.com>
2023-04-14 22:00:25 -0300

    Update user.repository.ts

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -2,22 +2,22 @@ import { IUser, User } from "../models/user.model";
 
 class UserRepository {
   getAll() {
-    return User.find();
+    return User.find({});
   }
 
   getByUsername(username: String) {
     return User.findOne({ username: username });
   }
 
-  createUser(user: IUser) {
+  create(user: IUser) {
     return User.create(user);
   }
 
-  updateUser(username: String, user: Partial<IUser>) {
+  update(username: String, user: Partial<IUser>) {
     return User.updateOne({ username: username }, { $set: user });
   }
 
-  removeUser(username: String) {
+  remove(username: String) {
     return User.deleteOne({ username: username });
   }
 }
